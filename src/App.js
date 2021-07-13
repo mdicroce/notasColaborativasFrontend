d8f72cf6a38119d89b323a8911eed4938c82666e
mdicroce <50848313+mdicroce@users.noreply.github.com>
2021-07-13 03:03:40 -0300

    unos pequeños cambios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React, { useState, useEffect } from 'react'
 import './App.css';
-import Login from './components/login'
+import Notes from './components/noteView'
+
+
+const notasAuxiliares = 
+    {
+        "title" : "Revisar la vista de notas",
+        "content": "Las vistas de notas tienen que poder visualizarse de forma correcta",
+        "room" : {
+            owner: {
+
+            },
+            users:{
+
+            },
+            notes: {
+
+            }
+        },
+        "responsable" : {
+                "username" : "mbexel",
+                "mail" : "mtt1pt@gmail.com"
+        },
+        "owner" : {
+                "username" : "mbexel",
+                "mail" : "mtt1pt@gmail.com"
+        },
+        "complete" : false,
+        "date": new Date().toISOString(),
+        "moreNotes":[]
+    }
 
 function App() {
   const {viewToShow, setViewToShow} = useState('frontPage')
-  
+  console.log(notasAuxiliares.moreNotes.length)
   return (
     <div>
-      <Login/>
-      {window.localStorage.getItem('loggedUser')}
+      <Notes notes={notasAuxiliares}/>
     </div>
     )
 }
--- src/components/noteView.js
+++ src/components/noteView.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+
+
 const Notes = (props)  => {
     const notas = <Note item = {props.notes}/>
     return (
@@ -11,6 +13,7 @@ const Notes = (props)  => {
 
 const Note = ({item}) => {
     let moreNotes = []
+    console.log(item)
     if(item.moreNotes.length > 0)
     {
         moreNotes = item.moreNotes.map((actual) => {
@@ -21,11 +24,11 @@ const Note = ({item}) => {
             <div>
                 <div key={item.id}>
                     <div><h2>Title: <span>{item.title}</span></h2>
-                        <h3>Owner: <span>item.owner.username</span></h3>
-                        <p>Responsable: <span>item.owner.username</span></p>
-                        <p>Fecha: <span>item.date</span></p>
+                        <h3>Owner: <span>{item.owner.username}</span></h3>
+                        <p>Responsable: <span>{item.owner.username}</span></p>
+                        <p>Fecha: <span>{item.date}</span></p>
                     </div>
-                    <textarea>item.content</textarea>
+                    <textarea>{item.content}</textarea>
 
                 </div>
                 <div className= "comments">
